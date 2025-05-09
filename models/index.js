735a04d1f39323f80de8099816b92a1a226f70ce
Aymeric <51028526+comanezz@users.noreply.github.com>
2025-05-09 15:14:10 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -6,6 +6,12 @@ const Sequelize = require("sequelize");
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
+
+config.host = process.env.DB_HOST || 'localhost';
+config.username = process.env.DB_USER || config.username;
+config.password = process.env.DB_PASS || config.password;
+config.database = process.env.DB_NAME || config.database;
+
 let db = {};
 let sequelize;
 if (config.use_env_variable) {
